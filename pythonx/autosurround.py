01713d02f87764108f2a89be92295da40a0fd33a
Stanislav Seletskiy <s.seletskiy@gmail.com>
2016-05-19 11:54:28 +0600

    experimental autosurround quotes

--- pythonx/autosurround.py
+++ pythonx/autosurround.py
@@ -69,8 +69,8 @@ def correct_inserted_pair(open, close):
         if close_pair_pos != (cursor[0], cursor[1]):
             corrected = True
             buffer[cursor[0]-1] = \
-                buffer[cursor[0]-1][:close_pair_pos[1]-1] + \
-                buffer[cursor[0]-1][close_pair_pos[1]:]
+                buffer[cursor[0]-1][:close_pair_pos[1]] + \
+                buffer[cursor[0]-1][close_pair_pos[1]+1:]
             del _current_pairs[open_pair]
 
     return corrected
@@ -86,11 +86,28 @@ def clean_current_pairs():
     for open_pair in _current_pairs:
         open_pair_pos = open_pair[0]
         close_pair_pos = _current_pairs[open_pair]
-
         if open_pair_pos <= cursor <= close_pair_pos:
             kept_pairs[open_pair] = close_pair_pos
 
     _current_pairs = kept_pairs
+
+
+def _match_enclosing_quote(cursor):
+    line = vim.current.buffer[cursor[0]-1][cursor[1]:]
+    match = re.match(r"(['\"`]).*", line)
+    if not match:
+        return
+
+    if _is_cursor_in_string(cursor):
+        return
+
+    if not _is_cursor_in_string((cursor[0], cursor[1]+1)):
+        return
+
+    with _restore_selection_register():
+        with _restore_cursor():
+            vim.command("normal! va{}\033".format(match.group(1)))
+            return vim.current.window.cursor
 
 
 def _match_enclosing_brace(cursor):
@@ -99,7 +116,7 @@ def _match_enclosing_brace(cursor):
     if not match:
         return
 
-    if _is_cursor_in_string():
+    if _is_cursor_in_string(cursor):
         return
 
     with _restore_cursor():
@@ -110,7 +127,7 @@ def _match_enclosing_brace(cursor):
 
 
 def _match_end_of_code_block(cursor):
-    if _is_cursor_in_string():
+    if _is_cursor_in_string(cursor):
         return
 
     if len(vim.current.buffer[cursor[0]-1]) == cursor[1]:
@@ -141,8 +158,20 @@ def _restore_cursor():
     vim.current.window.cursor = cursor
 
 
-def _is_cursor_in_string():
-    for syn_id in vim.eval('synstack(line("."), col("."))'):
+@contextmanager
+def _restore_selection_register():
+    cursor = vim.current.window.cursor
+    yield
+    vim.current.window.cursor = cursor
+
+
+def _is_cursor_in_string(cursor):
+    synstack = vim.eval('synstack({}, {})'.format(
+        cursor[0],
+        cursor[1]
+    ))
+
+    for syn_id in synstack:
         syn_name = vim.eval(
             'synIDattr(synIDtrans({}), "name")'.format(syn_id)
         )
@@ -155,3 +184,4 @@ def _is_cursor_in_string():
 enclosing_strategies = []
 register_finder(_match_end_of_code_block)
 register_finder(_match_enclosing_brace)
+register_finder(_match_enclosing_quote)
