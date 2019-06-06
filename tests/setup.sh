aac95a13b1eedfee21c58e151a0deb44901121e6
Egor Kovetskiy <e.kovetskiy@gmail.com>
2019-06-06 22:48:45 +0300

    experiment with gifs

--- tests/setup.sh
+++ tests/setup.sh
@@ -7,6 +7,19 @@ vim-tests:start "$_vim_session" < autosurround.vimrc
 
 vim-tests:type "$_vim_session" ":py autosurround.enable_debug_log()" "enter"
 
+:gif-next-id() {
+    local last_id=$(cd $_gifs_dir && ls -t | head -n1 | cut -d. -f1)
+    if [[ ! "$last_id" ]]; then
+        echo $_gifs_dir/1
+    fi
+    echo $_gifs_dir/$((last_id + 1))
+}
+
+record-terminal -n "$(:gif-next-id)" -s "$_vim_session" tmux attach -t "$_vim_session" &
+_gif_record=$!
+
+sleep 1
+
 :testcase() {
     cat > table
     csplit \
--- tests/teardown.sh
+++ tests/teardown.sh
@@ -1,3 +1,5 @@
 tests:eval vim-tests:get-messages "$_vim_session"
 
 vim-tests:end-silent "$_vim_session"
+
+wait $_gif_record
